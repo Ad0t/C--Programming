d3b56ebdb4d5f349c1320c8531633b13343ca9b7
Ad0t <171239099+Ad0t@users.noreply.github.com>
2025-01-18 19:58:22 +0530

    selection-sort

--- Sorting-Algo/1-selection-sort.cpp
+++ Sorting-Algo/1-selection-sort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void selectionSort (int arr[], int n) {
     for (int i=0; i<=n-2; i++) {
         int minIndex = i;
-        for (int j = i; j <= n-1; j++) {
+        for (int j = i + 1; j <= n-1; j++) {
             if (arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
